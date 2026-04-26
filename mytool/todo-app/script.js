d7725ecb0b32548ba85b63d969e7d393696925e0
workingpoormann <working.poor.system.programming@gmail.com>
2026-04-26 22:53:59 +0900

    feat: add initial TodoManger Implementation (WIP)

--- mytool/todo-app/script.js
+++ mytool/todo-app/script.js
@@ -12,11 +12,49 @@ class Todo {
     this.#sec = sec;
   }
 
-  get todoId() {
+  get id() {
     return this.#id;
   }
 
-  set todoId(id) {
+  set id(id) {
     this.#id = id;
+  }
+}
+
+class TodoManager {
+  #todos = [];
+
+  constructor() {
+    // load todos from locastorage
+  }
+
+  getTodos() {
+    return this.#todos;
+  }
+
+  /**
+   * Get a todo by id
+   * @param {number} id
+   * @returns Todo or undefined
+   */
+  getTodo(id) {
+    return this.#todos.find((todo) => {
+      return todo.id === id;
+    });
+  }
+
+  addTodo(text, hour, min, sec) {
+    const todo = new Todo(text, hour, min, sec);
+    if (this.#todos.length === 0) {
+      todo.id = 1;
+    } else {
+      todo.id = this.#todos.at(-1).id + 1;
+    }
+
+    this.#todos.push(todo);
+  }
+
+  deleteTodo(id) {
+    this.#todos = this.#todos.filter((todo) => todo.id !== id);
   }
 }
